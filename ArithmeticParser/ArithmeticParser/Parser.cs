d80f00e7d20764747e2a89da331b508932a6a4cb
Tom Humphreys <Tom01098Code@gmail.com>
2019-01-18 19:29:28 +0000

    Fully implemented the parser ! So now it can do chains of operations with integers and +, -, *

--- ArithmeticParser/ArithmeticParser/Parser.cs
+++ ArithmeticParser/ArithmeticParser/Parser.cs
@@ -28,21 +28,63 @@ namespace ArithmeticParser
         /// <returns>
         /// The result of parsing the tokens
         /// </returns>
-        public int Parse()
-        {
-            throw new NotImplementedException();
-        }
+        public int Parse() => Expression();
 
         // Expression := Integer {Operator Integer}
         private int Expression()
         {
-            throw new NotImplementedException();
+            tokens.MoveNext();
+            var result = Integer();
+
+            while (tokens.MoveNext())
+            {
+                var op = Operator();
+                tokens.MoveNext();
+                var num = Integer();
+
+                if (op is AddToken)
+                {
+                    result += num;
+                }
+                else if (op is SubtractToken)
+                {
+                    result -= num;
+                }
+                else if (op is MultiplyToken)
+                {
+                    result *= num;
+                }
+                else
+                {
+                    throw new ArgumentException("Unsupported operator");
+                }
+            }
+
+            return result;
+        }
+
+        // Operator := '+' | '-' | '*'
+        private OperatorToken Operator()
+        {
+            if (tokens.Current is OperatorToken op)
+            {
+                return op;
+            }
+
+            throw new ArgumentException(
+                $"Expected operator but got {tokens.Current}");
         }
 
         // Integer := Digit{Digit}
         private int Integer()
         {
-            throw new NotImplementedException();
+            if (tokens.Current is IntegerToken)
+            {
+                return ((IntegerToken)tokens.Current).Value;
+            }
+
+            throw new ArgumentException(
+                $"Expected integer but got {tokens.Current}");
         }
     }
 }
